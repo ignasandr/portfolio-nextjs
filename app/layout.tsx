32e1b3aa840b82e7e4fe442fda44e03c12187822
ignaskmn <ignas@kmn.lt>
2024-02-09 13:19:35 +0200

    Update body background color and text color

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* <body className={`${gupter.className} h-full`}> */}
-      <body className="h-full">
+      <body className="h-full bg-white text-black">
         <div className="flex w-full flex-col justify-center h-full">
           <div className="md:mx-4 lg:mx-24 xl:mx-48 2xl:mx-72">
             <Header />
